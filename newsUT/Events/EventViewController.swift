5281385ff63daa1a550e038142975251f10226b6
Yash K <33426213+YashKakodkar@users.noreply.github.com>
2019-05-11 16:50:44 -0500

    made outlets needed

--- newsUT/Events/EventViewController.swift
+++ newsUT/Events/EventViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class EventViewController: UIViewController {
 
+    @IBOutlet weak var eventImage: UIImageView!
+    @IBOutlet weak var eventTitle: UILabel!
+    @IBOutlet weak var eventMonth: UILabel!
+    @IBOutlet weak var eventDate: UILabel!
+    @IBOutlet weak var organizationName: UILabel!
+    @IBOutlet weak var organizationImage: UIImageView!
+    
+    @IBOutlet weak var dateTimeBUtton: UIButton!
+    @IBOutlet weak var locationButton: UIButton!
+    @IBOutlet weak var ticketButton: UIButton!
+    @IBOutlet weak var aboutEvent: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +30,12 @@ class EventViewController: UIViewController {
     }
     
 
+    @IBAction func onDateTime(_ sender: Any) {
+    }
+    @IBAction func onLocation(_ sender: Any) {
+    }
+    @IBAction func onTicket(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
