f5493a95d0eb1ff2d1afe2025a5d8b93c72b7082
Yash K <33426213+YashKakodkar@users.noreply.github.com>
2019-05-12 19:32:12 -0500

    implemented save button switch

--- newsUT/Events/EventCell.swift
+++ newsUT/Events/EventCell.swift
@@ -14,11 +14,50 @@ class EventCell: UITableViewCell {
     @IBOutlet weak var timedateLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var eventLabel: UILabel!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    var saved:Bool = false
+
+    @IBAction func onSave(_ sender: Any) {
+    
+        let toBeSaved = !saved
+        if(toBeSaved){
+            //TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setSaved(isSaved: true)
+            //}, failure: { (error) in
+                //print("Favorite did not succeed. Errror: \(error)")
+            //})
+        }else{
+            //TwitterAPICaller.client?.unFavoriteTweet(tweetId: tweetId, success: {
+                self.setSaved(isSaved: false)
+           // }, failure: { (error) in
+                //print("Unfavorite did not succeed. Errror: \(error)")
+            //})
+        }
+    
+    
+    
+    }
+    
+    
+    func setSaved(isSaved:Bool){
+        saved = isSaved
+        if(saved){
+            saveButton.setImage(UIImage(named:
+                "Saved_Filled"), for: UIControl.State.normal)
+        }else{
+            saveButton.setImage(UIImage(named:
+                "Saved"), for: UIControl.State.normal)
+        }
+    }
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
